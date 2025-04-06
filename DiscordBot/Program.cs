574b56157c2bc33a0ccedc09c502bc5704590c3e
Ed <85958810+LimLife@users.noreply.github.com>
2025-04-06 18:01:09 +0300

    rework_build_app

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,38 +1,32 @@
-﻿using Discord;
-using DotNetEnv;
-using DiscordBot.Host.Config;
-using DiscordBot.Host.Service;
-using DiscordBot.Host.Handlers;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using DiscordBot.Text.Extensions;
+using DiscordBot.Host.Handlers;
+using DiscordBot.Host.Service;
+using DiscordBot.Host.Config;
 using DiscordBot.Core;
-using DiscordBot.Core.Text;
-using DiscordBot.Modules.Text.Command;
+using DotNetEnv;
+using Discord;
 
 Env.TraversePath().Load();
 
-var builder = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((context, services) =>
-    {
-        services.AddSingleton(new BotConfig
-        {
-            Token = Env.GetString("DISCORD_TOKEN") ??
-                    throw new ArgumentNullException("DISCORD_TOKEN is not configured"),
-            Intents = GatewayIntents.Guilds | GatewayIntents.GuildMessages | GatewayIntents.MessageContent,
-            LogLevel = LogSeverity.Debug
-        });
-        services.AddSingleton<ICommandMapper, CommandMapper>();
-        services.AddSingleton<MessageHandler>();
-        services.AddSingleton<DiscordBotService>();
-        services.AddSingleton<LogHandler>();
-        services.AddHostedService<DiscordBotService>();
+var builder = new HostApplicationBuilder(args);
 
-        services.AddDiscordCommandHandlers();
-    })
-    .Build();
+builder.Services.AddSingleton(new BotConfig
+{
+    Token = Env.GetString("DISCORD_TOKEN") ??
+            throw new ArgumentNullException("DISCORD_TOKEN is not configured"),
+    Intents = GatewayIntents.Guilds | GatewayIntents.GuildMessages | GatewayIntents.MessageContent,
+    LogLevel = LogSeverity.Debug
+});
 
+builder.Services.AddSingleton<ICommandMapper, CommandMapper>();
+builder.Services.AddSingleton<MessageHandler>();
+builder.Services.AddSingleton<DiscordBotService>();
+builder.Services.AddSingleton<LogHandler>();
+builder.Services.AddHostedService<DiscordBotService>();
 
-await builder.RunAsync();
+builder.Services.AddDiscordCommandHandlers();
 
-
+var app = builder.Build();
+await app.RunAsync();
