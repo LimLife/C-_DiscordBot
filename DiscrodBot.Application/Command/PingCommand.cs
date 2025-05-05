dbdd94255b247dbbba203ee99c756247ecd6435e
Ed <85958810+LimLife@users.noreply.github.com>
2025-05-05 17:08:51 +0300

    rename lib

---
+++ DiscrodBot.Application/Command/PingCommand.cs
@@ -0,0 +1,29 @@
+﻿using DiscordBot.Core.Text;
+
+namespace DiscordBot.Modules.Text.Command
+{
+    public class PingCommand : IPingCommand
+    {
+        /*
+        private static readonly string[] _commands = { "ping", "help", "status" };
+        public IEnumerable<string> GetSupportedCommands() => _commands;
+
+        public async Task ExecuteAsync(string command, IMessageContext context)
+        {
+            switch (command.ToLower())
+            {
+                case "ping":
+                    await context.ReplyAsync("Pong");
+                    break;
+                default:
+                    await context.ReplyAsync("Exaction message");
+                    break;
+            }
+        }
+        */
+        public Task<string> GetPingResponseAsync()
+        {
+            return Task.FromResult("pong");
+        }
+    }
+}
---
+++ DiscrodBot.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using DiscordBot.Text.Command;
+using DiscordBot.Core;
+using DiscordBot.Modules.Text.Command;
+using DiscordBot.Core.Text;
+
+namespace DiscordBot.Text.Extensions
+{
+    public static class DiscordServiceExtensions
+    {
+        /// <summary>
+        /// Регистрирует обработчики текстовых команд, такие как 
+        /// <see cref="PingCommand"/> и другие.
+        /// </summary>
+        /// <param name="services">Сервис-контейнер DI</param>
+        /// <returns>Обновлённый <see cref="IServiceCollection"/></returns>
+        /// <remarks>
+        /// Реализованные команды:
+        /// <list type="bullet">
+        ///   <item><see cref="IPingCommand"/> → <see cref="PingCommand"/></item>
+        ///   <item><see cref="IServiceText"/> → <see cref="HelloWorldCommand"/></item>
+        ///   <item><see cref="IServiceText"/> → <see cref="DotaCommand"/></item>
+        /// </list>
+        /// </remarks>
+        public static IServiceCollection AddDiscordTextCommandHandlers(this IServiceCollection services)
+        {
+            services.AddSingleton<IPingCommand, PingCommand>();
+           // services.AddSingleton<IServiceText, HelloWorldCommand>();
+           // services.AddSingleton<IServiceText, DotaCommand>();
+            
+            return services;
+        }
+    }
+
+}
